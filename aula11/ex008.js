62f0927022f41e492640080af29b8260802a1a31
julianodepaula <julianodepaula@gmail.com>
2021-12-14 12:14:13 -0300

    Final aula D12

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do carro é ${vel} km/h`)
+if (vel > 60){
+    console.log('Vc ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Use sempre o cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+    console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Vc é Brasileiro')
+} else {
+    console.log('Vc é Estrangeiro')
+}
