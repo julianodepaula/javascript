e930182b81375308dfe56b91a7ce1132edce2cc4
julianodepaula <julianodepaula@gmail.com>
2021-12-17 08:57:22 -0300

    Final aula 14

---
+++ aula13e14/ambiente.js
@@ -0,0 +1,16 @@
+/*
+var c = 1
+while(c <= 10){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 15)
+*/
+for (var c=1;c<=5;c++){
+    console.log(`Passo ${c}`)
+}
+
