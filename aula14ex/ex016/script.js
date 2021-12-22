09be094e2340ec9417228ba18a3acd2692e2cc42
julianodepaula <julianodepaula@gmail.com>
2021-12-22 09:11:42 -0300

    Exercicio 17 finalizado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -9,13 +9,20 @@ function contar() {
 
     if (i <= 0 || f <= 0 || p <= 0) {
         res.innerHTML = "Campos não podem ser menor ou igual a 0!"
-    } else {
+    }else {
         res.innerHTML = ""
 
         let c = i
-        while(c <= f){
-            res.innerHTML += ` ${c} ` + String.fromCodePoint(0x1F476 ,0x1F3FB)
-            c += p
+        if (c<=f){
+            while(c <= f){
+                res.innerHTML += ` ${c} ` + String.fromCodePoint(0x1F476 ,0x1F3FB)
+                c += p
+            }
+        }else {
+            while(f < c){
+                res.innerHTML += ` ${c} ` + String.fromCodePoint(0x1F476 ,0x1F3FB)
+                c -= p
+            }
         }
         res.innerHTML += "Fim"
     }
